07da7a6f736221a12297c1685761eef3a57e2b9f
claes <claes>
2003-12-22 15:59:34 +0000

    Adapted to V4.0

--- src/exp/com/src/os_linux/wb_open_db.sh
+++ src/exp/com/src/os_linux/wb_open_db.sh
@@ -3,23 +3,16 @@
 # This procedure is started from the navigator 'Open db' method for
 # DbConfig and DbTrace objects.
 #
-database=$1
-username=$2
-password=$3
-volume=$4
-title=$5
+username=$1
+password=$2
+database=$3
+title=$4
 
+set -o xtrace
 if [ -z $database ]; then
-  echo "Database id is missing"
+  echo "Database is missing"
   return
 fi
 
-source pwrp_env.sh setdb $database
-
-if [ -z $volume ]; then
-  echo "-- Opening database '$database'"
-  wb $username $password
-else
-  echo "-- Opening database '$database' volume '$volume'"
-  wb $username $password $volume
-fi
+echo "-- Opening volume '$database'"
+wb $username $password $database
