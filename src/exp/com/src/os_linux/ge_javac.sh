44b06f2510ae1ca97f53c3ed9d5ea99589b895a2
claes <claes>
2004-05-07 13:26:57 +0000

    Java frames inserted in web jarfile also

--- src/exp/com/src/os_linux/ge_javac.sh
+++ src/exp/com/src/os_linux/ge_javac.sh
@@ -43,6 +43,7 @@ fi
 
 if [ $function == "java" ]; then
   jarfile="$pwrp_lib/pwrp_"$systemname".jar"
+  jarfileweb="$pwrp_web/pwrp_"$systemname"_web.jar"
 
   if javac -d $pwrp_web -classpath $pwr_lib/pwr_rt.jar:$pwr_lib/pwr_jop.jar $pwrp_pop/$src_file
   then
@@ -50,27 +51,36 @@ if [ $function == "java" ]; then
     if [ ! -e $jarfile ]; then
       cd $pwrp_web
       jar -cf $jarfile ${src_file%.java}.class
-      rm ${src_file%.java}.class
     else
       cd $pwrp_web
       jar -uf $jarfile ${src_file%.java}.class
-      rm ${src_file%.java}.class
     fi
+    if [ ! -e $jarfileweb ]; then
+      cd $pwrp_web
+      jar -cf $jarfileweb ${src_file%.java}.class
+    else
+      cd $pwrp_web
+      jar -uf $jarfileweb ${src_file%.java}.class
+    fi
+    rm ${src_file%.java}.class
     files=`ls $pwrp_web/${src_file%.java}\\$*.class 2>&-`
     if [ ! -z "$files" ]; then
       # echo "Insert in archive $jarfile"
       jar -uf $jarfile ${src_file%.java}\$*.class
+      jar -uf $jarfileweb ${src_file%.java}\$*.class
       rm ${src_file%.java}\$*.class
     fi
     files=`ls $pwrp_pop/*.gif 2>&-`
     if [ ! -z "$files" ]; then
       cd $pwrp_pop
       jar -uf $jarfile *.gif
+      jar -uf $jarfileweb *.gif
     fi
     files=`ls $pwrp_pop/*.jpg 2>&-`
     if [ ! -z "$files" ]; then
       cd $pwrp_pop
       jar -uf $jarfile *.jpg
+      jar -uf $jarfileweb *.jpg
     fi
 
   fi
