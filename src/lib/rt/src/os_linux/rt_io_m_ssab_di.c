5ebcf80bf3ebd5a4518a6566af0147428ceabfff
claes <claes>
2005-06-23 12:34:18 +0000

    New ssab card classes with internal channels

---
+++ src/lib/rt/src/os_linux/rt_io_m_ssab_di.c
@@ -0,0 +1,191 @@
+/* rt_io_m_ssab_di.c -- io methods for ssab cards.
+   OS Linux
+   PROVIEW/R
+   Copyright (C) 1994 by Comator Process AB.  */
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "pwr.h"
+#include "rt_errh.h"
+#include "pwr_baseclasses.h"
+#include "pwr_basecomponentclasses.h"
+#include "pwr_ssaboxclasses.h"
+#include "rt_io_base.h"
+#include "rt_io_msg.h"
+#include "rt_io_filter_di.h"
+#include "rt_io_ssab.h"
+#include "rt_io_card_init.h"
+#include "rt_io_card_close.h"
+#include "rt_io_card_read.h"
+#include "qbus_io.h"
+
+
+/*----------------------------------------------------------------------------*\
+  
+\*----------------------------------------------------------------------------*/
+
+typedef struct {
+	unsigned int	Address[2];
+	int		Qbus_fp;
+	struct {
+	  pwr_sClass_Di *sop[16];
+	  void	*Data[16];
+	  pwr_tBoolean Found;
+	} Filter[2];
+} io_sLocal;
+
+typedef struct {
+	int		Qbus_fp;
+} io_sRackLocal;
+
+static pwr_tStatus IoCardInit (
+  io_tCtx	ctx,
+  io_sAgent	*ap,
+  io_sRack	*rp,
+  io_sCard	*cp  
+) 
+{
+  pwr_sClass_Ssab_BaseDiCard	*op;
+  io_sLocal 		*local;
+  int			i, j;
+
+  op = (pwr_sClass_Ssab_BaseDiCard *) cp->op;
+  local = calloc( 1, sizeof(*local));
+  cp->Local = local;
+
+  errh_Info( "Init of di card '%s'", cp->Name);
+
+  local->Address[0] = op->RegAddress;
+  local->Address[1] = op->RegAddress + 2;
+  local->Qbus_fp = ((io_sRackLocal *)(rp->Local))->Qbus_fp;
+
+  /* Init filter */
+  for ( i = 0; i < 2; i++)
+  {
+    /* The filter handles one 16-bit word */
+    for ( j = 0; j < 16; j++)
+      local->Filter[i].sop[j] = cp->chanlist[i*16+j].sop;
+    io_InitDiFilter( local->Filter[i].sop, &local->Filter[i].Found,
+		local->Filter[i].Data, ctx->ScanTime);
+  }
+
+  return 1;
+}
+
+
+/*----------------------------------------------------------------------------*\
+  
+\*----------------------------------------------------------------------------*/
+static pwr_tStatus IoCardClose (
+  io_tCtx	ctx,
+  io_sAgent	*ap,
+  io_sRack	*rp,
+  io_sCard	*cp  
+) 
+{
+  io_sLocal 		*local;
+  int			i;
+
+  local = (io_sLocal *) cp->Local;
+
+  errh_Info( "IO closing di card '%s'", cp->Name);
+
+  /* Free filter data */
+  for ( i = 0; i < 2; i++)
+  {
+    if ( local->Filter[i].Found)
+      io_CloseDiFilter( local->Filter[i].Data);
+  }
+  free( (char *) local);
+
+  return 1;
+}
+
+
+/*----------------------------------------------------------------------------*\
+  
+\*----------------------------------------------------------------------------*/
+static pwr_tStatus IoCardRead (
+  io_tCtx	ctx,
+  io_sAgent	*ap,
+  io_sRack	*rp,
+  io_sCard	*cp  
+) 
+{
+  io_sLocal 		*local;
+  pwr_tUInt16		data = 0;
+  pwr_sClass_Ssab_BaseDiCard	*op;
+  pwr_tUInt16		invmask;
+  pwr_tUInt16		convmask;
+  int			i;
+  int			sts;
+  qbus_io_read 		rb;
+
+  local = (io_sLocal *) cp->Local;
+  op = (pwr_sClass_Ssab_BaseDiCard *) cp->op;
+
+  for ( i = 0; i < 2; i++)
+  { 
+    if ( i == 0)
+    {
+      convmask = op->ConvMask1;
+      invmask = op->InvMask1;
+    }
+    else
+    {
+      convmask = op->ConvMask2;
+      invmask = op->InvMask2;
+      if ( !convmask)
+        break;
+      if ( op->MaxNoOfChannels == 16)
+        break;
+    }
+
+    rb.Address = local->Address[i];
+    sts = read( local->Qbus_fp, &rb, sizeof(rb));
+    data = (unsigned short) rb.Data;
+    if ( sts == -1)
+    {
+      /* Increase error count and check error limits */
+      op->ErrorCount++;
+
+      if ( op->ErrorCount == op->ErrorSoftLimit)
+        errh_Error( "IO Error soft limit reached on card '%s'", cp->Name);
+      if ( op->ErrorCount >= op->ErrorHardLimit)
+      {
+        errh_Error( "IO Error hard limit reached on card '%s', IO stopped", cp->Name);
+        ctx->Node->EmergBreakTrue = 1;
+        return IO__ERRDEVICE;
+      }
+      continue;
+    }
+
+    /* Invert */
+    data = data ^ invmask;
+
+    /* Filter ... */
+    if ( local->Filter[i].Found)
+      io_DiFilter( local->Filter[i].sop, &data, local->Filter[i].Data);
+
+    /* Move data to valuebase */
+    io_DiUnpackWord( cp, data, convmask, i);
+  }
+  return 1;
+}
+
+
+/*----------------------------------------------------------------------------*\
+  Every method to be exported to the workbench should be registred here.
+\*----------------------------------------------------------------------------*/
+
+pwr_dExport pwr_BindIoMethods(Ssab_Di) = {
+  pwr_BindIoMethod(IoCardInit),
+  pwr_BindIoMethod(IoCardClose),
+  pwr_BindIoMethod(IoCardRead),
+  pwr_NullMethod
+};
