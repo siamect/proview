a05fe3f245ad566fbff429ebfc5bfb8d20b4b963
claes <claes>
2006-01-12 16:19:17 +0000

    New module wb

--- wb/exe/wb/src/os_linux/link_rule.mk
+++ wb/exe/wb/src/os_linux/link_rule.mk
@@ -2,8 +2,8 @@ ifndef link_rule_mk
 link_rule_mk := 1
 
 link = $(ldxx) $(elinkflags) $(domap) -o $(export_exe) \
-	$(export_obj) $(objects) $(wb_msg_objs) $(rt_msg_objs) \
-	$(pwr_obj)/rt_io_user.o $(pwr_obj)/wb_provider.o \
+	$(export_obj) $(objects) $(wb_msg_eobjs) $(rt_msg_eobjs) \
+	$(pwr_eobj)/rt_io_user.o $(pwr_obj)/wb_provider.o \
 	-L/usr/X11R6/lib -L/usr/local/BerkeleyDB.4.0/lib \
 	-L/opt/gnome/lib \
 	-lpwr_wb -lpwr_rt -lpwr_ge -lpwr_flow -lpwr_glow -lpwr_co \
