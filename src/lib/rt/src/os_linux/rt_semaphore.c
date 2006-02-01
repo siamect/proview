3bc2f02176db7da2f4c4603fb65f21a585be4131
claes <claes>
2006-02-01 09:12:17 +0000

    Included Knizhnik license terms

--- src/lib/rt/src/os_linux/rt_semaphore.c
+++ src/lib/rt/src/os_linux/rt_semaphore.c
@@ -1,23 +1,27 @@
 /* 
- * Proview   $Id: rt_semaphore.c,v 1.3 2005-09-01 14:57:57 claes Exp $
+ * Proview   $Id: rt_semaphore.c,v 1.4 2006-02-01 09:12:17 claes Exp $
  * Copyright (C) 2005 SSAB Oxelösund AB.
  *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation, either version 2 of 
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License 
- * along with the program, if not, write to the Free Software 
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+ * Distribution terms
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the Software), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
 
-/* rt_sect.c */
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHOR OF THIS SOFTWARE BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 /*-< SEMAPHORE.C >--------------------------------------------------*--------*/
 /* POSIX.1b                   Version 1.0        (c) 1998  GARRET   *     ?  */
