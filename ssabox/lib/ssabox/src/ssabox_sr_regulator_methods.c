835bc8c459c62f2a624dfe3fd55f1d48650ef083
claes <claes>
2006-02-01 08:21:21 +0000

    GPL headera added

--- ssabox/lib/ssabox/src/ssabox_sr_regulator_methods.c
+++ ssabox/lib/ssabox/src/ssabox_sr_regulator_methods.c
@@ -1,4 +1,23 @@
-/*2005-09-14      File: SR_regulator_methods.c
+/* 
+ * Proview   $Id: ssabox_sr_regulator_methods.c,v 1.2 2006-02-01 08:21:21 claes Exp $
+ * Copyright (C) 2005 SSAB Oxelösund AB.
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 2 of 
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with the program, if not, write to the Free Software 
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ **/
+
+/*
 * Author: Jonas Haulin   email: jonas@haulin.se 
 * Contains internal and engine functions for antisway objects. 
 */
