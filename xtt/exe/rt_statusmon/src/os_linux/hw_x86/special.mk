bbae074ce978d26d0becbfa12a775f5ae144159b
claes <claes>
2007-05-14 12:42:29 +0000

    *** empty log message ***

--- xtt/exe/rt_statusmon/src/os_linux/hw_x86/special.mk
+++
@@ -1,9 +0,0 @@
-copy : $(inc_dir)/statusmon_nodelist.h \
-       $(inc_dir)/statusmon_nodelistnav.h
-
-$(inc_dir)/statusmon_nodelist.h : \
-	../../statusmon_nodelist.h
-
-$(inc_dir)/statusmon_nodelistnav.h : \
-	../../statusmon_nodelistnav.h
-
