f194fb04a9aba3a2abd43bf4e6b3e48f6c7d0f70
claes <claes>
2007-06-15 11:28:53 +0000

    *** empty log message ***

--- xtt/lib/ge/src/os_linux/lib_local.mk
+++ xtt/lib/ge/src/os_linux/lib_local.mk
@@ -4,18 +4,18 @@ lib_local_mk := 1
 vpath %.dat $(hw_source):$(os_source):$(co_source)
 
 l_copy := $(exe_dir)/pwr_ge_setup.dat $(src_dir)/ge_colors.dat
-l_clean_copy := clean_dat
+l_clean_copy :=
 
 clean_dat :
-	@ $(rm) $(rmflags) $(exe_dir)/pwr_ge_setup.dat
-	@ $(rm) $(rmflags) $(src_dir)/ge_colors.dat
+	@ if [ -e $(exe_dir)/pwr_ge_setup.dat ]; then $(rm) $(rmflags) $(exe_dir)/pwr_ge_setup.dat; fi
+	@ if [ -e $(src_dir)/ge_colors.dat ]; then $(rm) $(rmflags) $(src_dir)/ge_colors.dat; fi
 
-$(exe_dir)/pwr_ge_setup.dat : pwr_ge_setup.dat
-	@ $(log_export)
+$(exe_dir)/pwr_ge_setup.dat : ../../pwr_ge_setup.dat
+	@ echo "Copy $(source)"
 	@ $(cp) $(cpflags) $(source) $(target)
 
 $(src_dir)/ge_colors.dat : ge_colors.dat
-	@ $(log_export)
+	@ echo "Copy $(source)"
 	@ $(cp) $(cpflags) $(source) $(target)
 
 endif
