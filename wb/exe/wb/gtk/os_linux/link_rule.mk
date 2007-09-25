1170cc371e6a68fd94e851517000ee59aea3011e
claes <claes>
2007-09-25 13:37:16 +0000

    pwr_cnv added

--- wb/exe/wb/gtk/os_linux/link_rule.mk
+++ wb/exe/wb/gtk/os_linux/link_rule.mk
@@ -9,7 +9,7 @@ ifeq ($(export_type),exp)
 	-L/opt/gnome/lib \
 	-lpwr_wb_gtk -lpwr_wb -lpwr_wb_gtk -lpwr_wb -lpwr_rt_gtk -lpwr_rt \
         -lpwr_ge_gtk -lpwr_ge \
-	-lpwr_flow_gtk -lpwr_flow -lpwr_glow_gtk -lpwr_glow -lpwr_co_gtk -lpwr_co -lpwr_rt -lpwr_statussrv \
+	-lpwr_flow_gtk -lpwr_flow -lpwr_glow_gtk -lpwr_glow -lpwr_cnv -lpwr_co_gtk -lpwr_co -lpwr_rt -lpwr_statussrv \
 	`pkg-config --libs gtk+-2.0` \
 	-lpwr_msg_dummy -lantlr \
 	-lrpcsvc -lpthread -lm -ldb_cxx -lz
@@ -20,7 +20,7 @@ else
 	-L/usr/X11R6/lib -L/usr/local/BerkeleyDB.4.0/lib \
 	-L/opt/gnome/lib \
 	-lpwr_wb_gtk -lpwr_wb -lpwr_rt_gtk -lpwr_rt -lpwr_ge_gtk -lpwr_ge \
-	-lpwr_flow_gtk -lpwr_flow -lpwr_glow_gtk -lpwr_glow -lpwr_co_gtk -lpwr_co -lpwr_statussrv -lpwr_rt\
+	-lpwr_flow_gtk -lpwr_flow -lpwr_glow_gtk -lpwr_glow -lpwr_cnv -lpwr_co_gtk -lpwr_co -lpwr_statussrv -lpwr_rt\
 	`pkg-config --libs gtk+-2.0` \
 	-lpwr_msg_dummy -lantlr \
 	-lrpcsvc -lpthread -lm -ldb_cxx -lz
