abfd71ff1b13125191d370677a55833f9459ca29
claes <claes>
2007-09-25 11:20:24 +0000

    Moved to lib/cnv

--- src/exe/co_convert/src/os_linux/hw_x86/link_rule.mk
+++ src/exe/co_convert/src/os_linux/hw_x86/link_rule.mk
@@ -2,7 +2,7 @@ ifndef link_rule_mk
 link_rule_mk := 1
 
 link = $(ldxx) $(linkflags) $(domap) -o $(export_exe) $(export_obj) \
-	$(objects) -L/usr/X11R6/lib -L/opt/gnome/lib -lpwr_co \
+	$(objects) -L/usr/X11R6/lib -L/opt/gnome/lib -lpwr_cnv -lpwr_co \
 	`pkg-config --libs gtk+-2.0` \
 	-lX11 -lrt -lm
 
