32550b9d513d345373256b26215ba704e7be1119
claes <claes>
2007-11-26 13:10:31 +0000

    lisbusbio changed

--- src/exe/rt_io_comm/src/os_linux/link_rule.mk
+++ src/exe/rt_io_comm/src/os_linux/link_rule.mk
@@ -5,11 +5,11 @@ ifeq ($(export_type),exp)
   link = $(ld) $(explinkflags) $(domap) -o $(export_exe) \
 	$(export_obj) $(objects) $(rt_msg_objs) \
 	$(pwr_obj)/rt_io_user.o \
-	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lusbio -lpthread  -lm
+	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpthread  -lm
 else
   link = $(ld) $(elinkflags) $(domap) -o $(export_exe) \
 	$(export_obj) $(objects) $(rt_msg_objs) \
 	$(pwr_obj)/rt_io_user.o \
-	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lusbio -lpthread  -lm
+	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpthread  -lm
 endif
 endif
