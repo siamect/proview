07ecb4241afb510fb76821c02cb7a04b5697d492
claes <claes>
2008-04-07 14:57:39 +0000

    *** empty log message ***

--- xtt/exe/wb_ge/gtk/os_linux/link_rule.mk
+++ xtt/exe/wb_ge/gtk/os_linux/link_rule.mk
@@ -2,7 +2,7 @@ ifndef link_rule_mk
 link_rule_mk := 1
 
 link = $(ldxx) $(elinkflags) $(domap) -o $(pwr_exe)/wb_ge_gtk \
-	$(bld_dir)/wb_ge_gtk.o $(wb_msg_eobjs) $(rt_msg_eobjs) \
+	$(bld_dir)/wb_ge_gtk.o $(wb_msg_eobjs) $(rt_msg_eobjs) $(pwr_eobj)/pwr_msg_glow.o\
 	$(pwr_eobj)/rt_io_user.o $(pwr_eobj)/wb_provider.o -L/usr/X11R6/lib \
 	-L/usr/local/BerkeleyDB.4.0/lib \
 	-L/opt/gnome/lib \
