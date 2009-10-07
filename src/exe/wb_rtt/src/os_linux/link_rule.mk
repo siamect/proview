e3d3827686b1ea71b3266d6ecb979266c91b46d8
Claes Sjofors <claes.sjofors@proview.se>
2009-10-07 17:06:23 +0200

    wb_rtt link rules fixed

--- src/exe/wb_rtt/src/os_linux/link_rule.mk
+++ src/exe/wb_rtt/src/os_linux/link_rule.mk
@@ -3,7 +3,7 @@ link_rule_mk := 1
 
 link = $(ldxx) $(linkflags) $(domap) -o $(export_exe) \
 	$(export_obj) $(objects) $(rt_msg_objs)\
-	$(pwr_obj)/rt_io_user.o\
-	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lrpcsvc -lpthread -lm -lrt -lcrypt
+	$(pwr_obj)/rt_io_user.o -L$(pwr_elib) \
+	-lpwr_rt -lpwr_co -lpwr_flow -lpwr_msg_dummy -lrpcsvc -lpthread -lm -lrt -lcrypt
 
 endif
