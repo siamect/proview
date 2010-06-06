b851d56d347019797915eae0c6240059765714b3
claes <claes@claes-desktop.(none)>
2010-06-06 19:48:03 +0200

    IO methods and objects for temperature sensor Maxim DS18B20

---
+++ otherio/lib/rt/src/os_linux/rt_io_m_onewire.h
@@ -0,0 +1,11 @@
+/* ra_io_m_onewire.h -- I/O methods for class OneWire. */
+
+#ifndef ra_io_m_onewire_h
+#define ra_io_m_onewire_h
+
+typedef struct {
+  FILE *value_fp;
+  int interval_cnt;
+} io_sLocalDS18B20;
+
+#endif
