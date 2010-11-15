61af5e200c88041691f1fb5df9154120299dcff2
Claes Sjöfors <claes@Claes-Sjoforss-Mac-mini.local>
2010-11-15 21:22:55 +0100

    MacOS build fix

--- xtt/exe/wb_ge/gtk/os_macos/link_rule.mk
+++ xtt/exe/wb_ge/gtk/os_macos/link_rule.mk
@@ -4,8 +4,6 @@ link_rule_mk := 1
 link = $(ldxx) -L$(pwr_elib) $(elinkflags) $(domap) -o $(pwr_exe)/wb_ge_gtk \
 	$(bld_dir)/wb_ge_gtk.o $(wb_msg_eobjs) $(rt_msg_eobjs) $(pwr_eobj)/pwr_msg_glow.o\
 	$(pwr_eobj)/rt_io_user.o $(pwr_eobj)/wb_provider.o -L/usr/X11R6/lib \
-	-L/usr/local/BerkeleyDB.4.0/lib \
-	-L/opt/gnome/lib \
 	-lpwr_ge_gtk -lpwr_ge -lpwr_wb_gtk -lpwr_wb -lpwr_wb_gtk -l pwr_wb -lpwr_ge_gtk -lpwr_ge \
 	-lpwr_flow_gtk -lpwr_flow -lpwr_glow_gtk -lpwr_glow -lpwr_rt \
 	-lpwr_cow_gtk -lpwr_cow -lpwr_co -lpwr_flow_gtk -lpwr_flow \
