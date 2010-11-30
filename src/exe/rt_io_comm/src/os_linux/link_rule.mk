f34e26a7e02fe6de2639b48b23aa41d7c19f2444
Robert Karlsson <robertk@ssab.com>
2010-11-30 15:46:47 +0100

    Added library dependency

--- src/exe/rt_io_comm/src/os_linux/link_rule.mk
+++ src/exe/rt_io_comm/src/os_linux/link_rule.mk
@@ -2,14 +2,14 @@ ifndef link_rule_mk
 link_rule_mk := 1
 
 ifeq ($(export_type),exp)			 
-  link = $(ld) $(explinkflags) $(domap) -o $(export_exe) \
+  link = $(ldxx) $(explinkflags) $(domap) -o $(export_exe) \
 	$(export_obj) $(objects) $(rt_msg_objs) \
 	$(pwr_obj)/rt_io_user.o \
-	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpwr_usb_dummy -lpthread  -lm
+	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpwr_usb_dummy -lpthread  -lm -lpnioif
 else
-  link = $(ld) $(elinkflags) $(domap) -o $(export_exe) \
+  link = $(ldxx) $(elinkflags) $(domap) -o $(export_exe) \
 	$(export_obj) $(objects) $(rt_msg_objs) \
 	$(pwr_obj)/rt_io_user.o \
-	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpwr_usb_dummy -lpthread  -lm
+	-lpwr_rt -lpwr_co -lpwr_msg_dummy -lpwr_usbio_dummy -lpwr_usb_dummy -lpthread  -lm -lpnioif
 endif
 endif
