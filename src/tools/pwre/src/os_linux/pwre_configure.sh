f7f209df098ba7474d67c01fc5dfd2f1eced9b42
Claes <claes@silverarrow.(none)>
2011-06-19 19:58:17 +0200

    pwre config cifx_dummy added

--- src/tools/pwre/src/os_linux/pwre_configure.sh
+++ src/tools/pwre/src/os_linux/pwre_configure.sh
@@ -307,7 +307,7 @@ if test $pwre_hw == "hw_arm"; then
   echo "export pwre_conf_libpwrco=\"-lpwr_co\"" >> $cfile
   echo "export pwre_conf_libpwrrt=\"-lpwr_rt -lpwr_co -lpwr_statussrv -lpwr_msg_dummy\"" >> $cfile
   echo "export pwre_conf_libpwrdtt=\"-lpwr_dtt\"" >> $cfile
-  echo "export pwre_conf_libpwrotherio=\"-lpwr_usbio_dummy -lpwr_usb_dummy\"" >> $cfile
+  echo "export pwre_conf_libpwrotherio=\"-lpwr_usbio_dummy -lpwr_usb_dummy -lpwr_cifx_dummy\"" >> $cfile
   echo "export pwre_conf_libpwrprofibus=\"-lpwr_pnak_dummy\"" >> $cfile
   echo "export pwre_conf_libpwrxtt=\"-lpwr_xtt -lpwr_ge -lpwr_cow -lpwr_flow -lpwr_glow\"" >> $cfile
   echo "export pwre_conf_libpwrxttgtk=\" -lpwr_xtt_gtk -lpwr_ge_gtk -lpwr_cow_gtk -lpwr_flow_gtk -lpwr_glow_gtk\"" >> $cfile
