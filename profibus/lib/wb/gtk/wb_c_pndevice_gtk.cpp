c0688e778fda4acb12a0ce2899bf98a0b922b80a
Claes Sjofors <claes.sjofors@proview.se>
2011-11-22 13:30:19 +0100

    Profinet Device method, CopyDevice, added

--- profibus/lib/wb/gtk/wb_c_pndevice_gtk.cpp
+++ profibus/lib/wb/gtk/wb_c_pndevice_gtk.cpp
@@ -141,6 +141,86 @@ static pwr_tStatus ConfigureFilter (
 }
 
 //
+// Copy Device.
+//
+static pwr_tStatus CopyDevice (
+  ldh_sMenuCall *ip
+)
+{
+  char *gsdml;
+  int size;
+  int sts;
+  pwr_tOid oid;
+  pwr_tAttrRef aref[2];
+  pwr_tFileName datafile_src, datafile_dest;
+  pwr_tFileName found_file;
+  pwr_tCmd cmd;
+
+  // Check if data file exist
+  sprintf( datafile_src, "$pwrp_load/pwr_pn_%s.xml", id_to_string( ip->Pointed.Objid));
+  dcli_translate_filename( datafile_src, datafile_src);
+
+  sts = dcli_search_file( datafile_src, found_file, DCLI_DIR_SEARCH_INIT);
+  dcli_search_file( datafile_src, found_file, DCLI_DIR_SEARCH_END);
+  if ( EVEN(sts)) return sts;
+
+  sts = ldh_GetObjectPar( ip->PointedSession, ip->Pointed.Objid, "RtBody",
+			  "GSDMLfile", &gsdml, &size);
+  if ( EVEN(sts)) return sts;
+  if ( strcmp( gsdml, "") == 0) {
+    free( gsdml);  
+    return 0;
+  }
+  free( gsdml);  
+
+  memset( aref, 0, sizeof(aref));
+  aref[0] = ip->Pointed;
+  sts = ldh_CopyObjectTrees( ip->PointedSession, aref, ip->Pointed.Objid,
+			     ldh_eDest_After, 0, 1, 0);
+  if ( EVEN(sts)) return sts;
+
+  sts = ldh_GetNextSibling( ip->PointedSession, ip->Pointed.Objid, &oid);
+  if ( EVEN(sts)) return sts;
+
+  sprintf( datafile_dest, "$pwrp_load/pwr_pn_%s.xml", id_to_string( oid));
+  dcli_translate_filename( datafile_dest, datafile_dest);
+
+  snprintf( cmd, sizeof(cmd), "cp %s %s", datafile_src, datafile_dest);
+  system( cmd);
+
+  return 1;
+}
+
+static pwr_tStatus CopyDeviceFilter (
+  ldh_sMenuCall *ip
+)
+{
+  char *gsdml;
+  int size;
+  int sts;
+  pwr_tFileName datafile;
+  pwr_tFileName found_file;
+
+  // Check if data file exist
+  sprintf( datafile, "$pwrp_load/pwr_pn_%s.xml", id_to_string( ip->Pointed.Objid));
+  dcli_translate_filename( datafile, datafile);
+
+  sts = dcli_search_file( datafile, found_file, DCLI_DIR_SEARCH_INIT);
+  dcli_search_file( datafile, found_file, DCLI_DIR_SEARCH_END);
+  if ( EVEN(sts)) return 0;
+
+  sts = ldh_GetObjectPar( ip->PointedSession, ip->Pointed.Objid, "RtBody",
+			  "GSDMLfile", &gsdml, &size);
+  if ( EVEN(sts)) return sts;
+  if ( strcmp( gsdml, "") == 0) {
+    free( gsdml);  
+    return 0;
+  }
+  free( gsdml);  
+  return 1;
+}
+
+//
 //  Syntax check.
 //
 
@@ -213,6 +293,8 @@ static pwr_tStatus SetIoDeviceData (
 pwr_dExport pwr_BindMethods(PnDevice) = {
   pwr_BindMethod(Configure),
   pwr_BindMethod(ConfigureFilter),
+  pwr_BindMethod(CopyDevice),
+  pwr_BindMethod(CopyDeviceFilter),
   pwr_BindMethod(SyntaxCheck),
   pwr_BindMethod(GetIoDeviceData),
   pwr_BindMethod(SetIoDeviceData),
