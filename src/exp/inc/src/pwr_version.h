132511389d5c52eafa778bb61f6eaaa709e09638
Claes Sjofors <claes.sjofors@proview.se>
2012-09-19 14:42:46 +0200

    Version 4.8.6

--- src/exp/inc/src/pwr_version.h
+++ src/exp/inc/src/pwr_version.h
@@ -88,10 +88,10 @@
 # define SET_VERSION(a, b, c, d)  ((pwr_tVersion)((a << 24) + (a << 16) + (a << 8) + a))		  
 #endif
 
-#define pwrv_cBuildTimeStr       "10-MAY-2012 09:00:00"
+#define pwrv_cBuildTimeStr       "12-SEP-2012 12:00:00"
 
-#define pwrv_cPwrVersionStr      "V4.8.5"
-#define pwrv_cPwrVersion         SET_VERSION('V', 4, 8, 5)
+#define pwrv_cPwrVersionStr      "V4.8.6"
+#define pwrv_cPwrVersion         SET_VERSION('V', 4, 8, 6)
 
 #define pwrv_cWbdbVersionShortStr "V48"
 #define pwrv_cPwrCopyright	 "Copyright (C) 2005-2012 SSAB EMEA AB"
