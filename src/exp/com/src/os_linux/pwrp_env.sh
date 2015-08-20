c52c3a3d35523d1377b66f315f02bc44d7698ef7
Claes Sjofors <claes.sjofors@proview.se>
2015-08-20 15:17:39 +0200

    pwrp_env.sh, minor change in login.sh

--- src/exp/com/src/os_linux/pwrp_env.sh
+++ src/exp/com/src/os_linux/pwrp_env.sh
@@ -609,6 +609,10 @@ pwrc_create_func()
 # Printer command for plc documents
 #export pwr_foe_gre_print="lpr -P lp1"
 
+# Specification of external directories with includefiles, included in the plc code
+# by ra_plc_user.h. The syntax is a compiler option string
+#export PWR_EXT_INC="-I/usr/local/include"
+
 if [ -e "\$pwrp_doc/sysinfo.txt" ]; then
   echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
   echo "Welcome to"
