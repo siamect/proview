f883b35fbe77ed6797cfe6e02a7b35c73223b9c7
Claes Sjofors <claes.sjofors@proview.se>
2017-09-07 14:08:07 +0200

    errlog message added when IO stalled by cyclesup

--- src/lib/rt/src/rt_plc_thread.c
+++ src/lib/rt/src/rt_plc_thread.c
@@ -705,9 +705,12 @@ scan (
 	time_GetTime(&now);
 	delay_action = csup_Exec(&sts, tp->csup_lh, (pwr_tDeltaTime *) &tp->sync_time, (pwr_tDeltaTime *) &tp->after_scan, &now);
 	if (delay_action == 2) {
+	  int prev_rwflag = pp->IOHandler->IOReadWriteFlag;
 	  pp->IOHandler->IOReadWriteFlag = FALSE;
 	  pp->Node->EmergBreakTrue = TRUE;
 	  errh_SetStatus( PLC__IOSTALLED);
+	  if ( prev_rwflag)
+	    errh_Fatal("CycleSup delayed with action Stop I/O");	  
 	}
       }
 
