35a0b6a2aa391dd55620b25553361aee1a287634
Christoffer Ackelman <christoffer.ackelman@ssab.com>
2018-10-10 11:55:42 +0200

    Qt: Minor style change on rt_xtt.

--- xtt/exe/rt_xtt/src/rt_xtt.cpp
+++ xtt/exe/rt_xtt/src/rt_xtt.cpp
@@ -65,12 +65,18 @@ int main(int argc, char* argv[])
   if (!found) {
     struct stat st;
 
-    strcpy(file, "$pwr_exe/rt_xtt_gtk");
+    strcpy(file, "$pwr_exe/rt_xtt_qt");
     dcli_translate_filename(file, file);
     if (stat(file, &st) == 0)
-      strcpy(wmg, "gtk");
-    else
-      strcpy(wmg, "motif");
+      strcpy(wmg, "qt");
+    else {
+      strcpy(file, "$pwr_exe/rt_xtt_gtk");
+      dcli_translate_filename(file, file);
+      if (stat(file, &st) == 0)
+        strcpy(wmg, "gtk");
+      else
+        strcpy(wmg, "motif");
+    }
   }
 
   strcpy(file, argv[0]);
