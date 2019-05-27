7f9c50f00bd234cbd2a40e68f2aed1e8bb70e378
Christoffer Ackelman <christoffer.ackelman@ssab.com>
2019-05-27 14:56:44 +0200

    Web: Added Babel to the repo, so that the client doesn't need to download it.

--- src/tools/bld/src/jsw_generic.mk
+++ src/tools/bld/src/jsw_generic.mk
@@ -21,6 +21,7 @@ source_dirs := $(co_source)
 -include jsw_local.mk
 
 vpath %.js $(co_source)
+vpath %.ts $(co_source)
 vpath %.html $(co_source)
 vpath %.png $(co_source)
 
@@ -29,6 +30,15 @@ js_sources := $(sort \
                $(foreach dir, \
                  $(source_dirs), \
                  $(wildcard $(dir)/*.js) \
+               ), $(notdir $(file)) \
+             ) \
+           )
+
+ts_sources := $(sort \
+             $(foreach file, \
+               $(foreach dir, \
+                 $(source_dirs), \
+                 $(wildcard $(dir)/*.ts) \
                ), $(notdir $(file)) \
              ) \
            )
@@ -61,10 +71,12 @@ css_sources := $(sort \
            )
 
 export_js := $(addprefix $(web_dir)/,$(js_sources))
+export_ts := $(addprefix $(web_dir)/,$(ts_sources))
 export_html := $(addprefix $(web_dir)/,$(html_sources))
 export_png := $(addprefix $(web_dir)/,$(png_sources))
 export_css := $(addprefix $(web_dir)/,$(css_sources))
 clean_js := $(patsubst %.js,clean_%.js, $(js_sources))
+clean_ts := $(patsubst %.ts,clean_%.ts, $(ts_sources))
 clean_html := $(patsubst %.html,clean_%.html, $(html_sources))
 clean_png := $(patsubst %.png,clean_%.png, $(png_sources))
 clean_css := $(patsubst %.css,clean_%.css, $(css_sources))
@@ -75,13 +87,13 @@ all : init copy lib exe | silent
 
 init :
 
-copy : $(export_js) $(export_html) $(export_png) $(export_css) | silent
+copy : $(export_js) $(export_ts) $(export_html) $(export_png) $(export_css) | silent
 
 lib :
 
 exe :
 
-clean : $(clean_js) $(clean_html) $(clean_png) $(clean_css)
+clean : $(clean_js) $(clean_ts) $(clean_html) $(clean_png) $(clean_css)
 
 clean_copy: $(clean)
 
@@ -93,6 +105,10 @@ silent:
 $(clean_js) : clean_%.js : %.js
 	@ echo "Removing js: $(web_dir)/$*.js"
 	@ $(rm) $(rmflags) $(web_dir)/$*.js
+
+$(clean_ts) : clean_%.ts : %.ts
+	@ echo "Removing ts: $(web_dir)/$*.ts"
+	@ $(rm) $(rmflags) $(web_dir)/$*.ts
 
 $(clean_html) : clean_%.html : %.html
 	@ echo "Removing html: $(web_dir)/$*.html"
