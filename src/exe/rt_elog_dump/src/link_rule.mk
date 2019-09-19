c9ee7d02ddf0ef59a61f343ec9aaa4017adcc43a
Christoffer Ackelman <christoffer.ackelman@ssab.com>
2019-09-19 08:51:10 +0200

    Fixed linking error on newer GCC versions.

--- src/exe/rt_elog_dump/src/link_rule.mk
+++ src/exe/rt_elog_dump/src/link_rule.mk
@@ -1,9 +1,8 @@
 ifndef link_rule_mk
 link_rule_mk := 1
 
-link = $(ld) $(linkflags) $(domap) -o $(export_exe) \
-	$(export_obj) $(objects) $(rt_msg_objs) \
-	$(pwr_obj)/rt_io_user.o \
-	$(pwre_conf_libdir) $(pwre_conf_libpwrrt) $(pwre_conf_lib)	
+link = $(ld) $(domap) -o $(export_exe) $(export_obj) $(objects) \
+	$(rt_msg_objs) $(pwr_obj)/rt_io_user.o $(pwre_conf_libdir) \
+	$(pwre_conf_libpwrrt) $(pwre_conf_lib) $(linkflags)
 
 endif
--- src/tools/bld/src/link_rule_generic.mk
+++ src/tools/bld/src/link_rule_generic.mk
@@ -1,8 +1,7 @@
 ifndef link_rule_mk
 link_rule_mk := 1
 
-link = $(ld) $(elinkflags) $(domap) -o $(export_exe) \
-	$(export_obj) $(objects) $(rt_msg_objs) \
-	$(pwre_conf_libdir) $(pwre_conf_libpwrrt) $(pwre_conf_lib)
+link = $(ld) $(domap) -o $(export_exe) $(export_obj) $(objects) $(rt_msg_objs) \
+	$(pwre_conf_libdir) $(pwre_conf_libpwrrt) $(pwre_conf_lib) $(elinkflags)
 
 endif
